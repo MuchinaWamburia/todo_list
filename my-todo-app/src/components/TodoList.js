da919bb160132f61208129bc4c0f52e60c411896
MuchinaWamburia <lucymuchina20@gmail.com>
2023-03-01 11:47:28 +0300

    create_react app

---
+++ my-todo-app/src/components/TodoList.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+const TodoList = () => {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api/todos')
+      .then(response => {
+        setTodos(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
+  const handleDelete = (id) => {
+    axios.delete(`/api/todos/${id}`)
+      .then(response => {
+        setTodos(todos.filter(todo => todo.id !== id));
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <ul>
+        {todos.map(todo => (
+          <li key={todo.id}>
+            <Link to={`/todos/${todo.id}`}>{todo.title}</Link>
+            <button onClick={() => handleDelete(todo.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+      <Link to="/todos/new">Add Todo</Link>
+    </div>
+  );
+};
+
+export default TodoList;
